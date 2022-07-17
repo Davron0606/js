8d6bfbe680c593fb670c6d9a15b6648a2c60ef13
Davron <ergashevdavron948@gmail.com>
2022-07-17 15:57:08 +0300

    Davron

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var ism = prompt ('ismingizni kiriting')
+console.log('salom mening ismim ' + ism);
+var yosh = +prompt ('yoshingizni kiriting')
+console.log('mening yoshim '+ yosh + ' da' )
+var summa1 = +prompt ('misolni yeching' + ' 6-3=? ');
+console.log('Misol 1: 6-3=3 ' + ' sizning javob ' + summa1);
+var summa2 = +prompt ('misolni yeching' + ' 4+10=? ');
+console.log('Misol 2: 4+10=14 ' + ' sizning javob ' + summa2);
+var summa3 = +prompt ('misolni yeching' + ' 9/3=? ');
+console.log('Misol 3: 9/3=3 ' + ' sizning javob ' + summa3);
+var summa4 = +prompt ('misolni yeching' + '2*5=? ');
+console.log('Misol 4: 2*5=10 ' + ' sizning javob ' + summa4);
+var summa5 = +prompt ('misolni yeching' + '20 % 5=? ');
+console.log('Misol 5: 20 % 5=0 ' + ' sizning javob ' + summa5);
+alert('javobni korish uchun console ni oching')
